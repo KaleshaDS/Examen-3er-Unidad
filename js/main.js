38fc974a05de34558d1400d4a68e30376922f0d8
KaleshaDS <dedi.kalesha@gmail.com>
2016-06-13 20:20:25 -0500

    funcionalidadlv0

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
-function check(input){
-
-    console.log("check"+input.nodeName)
-
-    
-    
-    if(this.checked){
-            nombre.className="hecho"
-        }else{
-            nombre.className=""
-        }
-
-}
-
+//Funcion que envia a la ventana de chat el mensaje del usuario//
 function add() {
-    var msn = document.createElement('li');
-    msn.className = 'row';
-    //pasar lo del input a la variable salida
-    var nombre = document.getElementById('chatLine').value;
-    li.innerHTML = '<div class="p-chat" onclick="check(this)"> ' + nombre +'</div>  <span class="text-mutted pull-right"></span>' ;
-     document.getElementById('mensajes').appendChild(li);
-     console.log ("holi");
+   var li = document.createElement('li');
+   li.className = 'row';
+   var msn = document.getElementById('intro').value;
+   li.innerHTML ='<div class= "r-chat">'+ msn + '<span>'+moment().format("HH:mm")+'</span>'+'</div';
+   document.getElementById('mensajitos').appendChild(li);
 }
+//Limpiar el imput #intro cuando se envia el mensaje//
+$('#send').click(function(){
+   var texto = $('#intro').val();
+   $('#intro').val('');
+});
+//Enviar mensaje cuando se presiona la tecla ENTER (#13)
+$('#intro').keypress(function(e){
+   if (e.which == 13) {   // 13 es el codigo de la tecla ENTER
+   var texto = $('#intro').val();
+   console.log(texto);
+   add (texto);
+   $('#intro').val('');
+   }
+}); 
 
+//Buscar contactos //
+function search(){
+   var table = document.getElementById('datos');
+   var searchText = document.getElementById('search').value.toLowerCase();
+   var cells="";
+   var found=false;
+   var compareWith="";
+// Recorremos todas las filas con contenido de la tabla
+   for (var i = 1; i < table.rows.length; i++){
+      cells = table.rows[i].getElementsByTagName('td');
+      found = false;
+// Recorremos todas las celdas
+   for (var j = 0; j < cells.length && !found; j++){
+   compareWith = cells[j].innerHTML.toLowerCase();
+// Buscamos el texto en el contenido de la celda
+   if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)){
+      found = true;
+      }
+   }
+   if(found){
+      table.rows[i].style.display = '';
+   } else {
+// si no ha encontrado ninguna coincidencia, esconde la fila de la tabla
+      table.rows[i].style.display = 'none';
+      }
+   }
+}
